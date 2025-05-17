29817afb508c44eecb614762dc489a0e66770932
A.Mohan <mohan6121@email.com>
2025-05-17 12:35:33 +0530

    mohan

--- src/database.py
+++ src/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
